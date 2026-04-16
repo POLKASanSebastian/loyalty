9fd4f23cf5251da7265be49859615e29cfc88820
POLKASanSebastian <inversportinver@gmail.com>
2026-04-16 16:38:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/loyalty/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        staff: 'staff.html',
+      }
+    }
+  }
 })
-
